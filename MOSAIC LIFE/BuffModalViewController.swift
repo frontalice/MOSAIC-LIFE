2973ba1940d4c1bb6729caadfa57d14014cd1c19
frontalice <mayonakaTV@me.com>
2021-05-06 22:53:07 +0900

    ver.1.2.11 ・日付ピッカーのデータをラベルに反映

--- MOSAIC LIFE/BuffModalViewController.swift
+++ MOSAIC LIFE/BuffModalViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class BuffModalViewController: UIViewController {
 
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        datePicker.addTarget(self, action: #selector(writeLimitDate), for: .valueChanged)
+//        datePicker.addTarget(self, action: #selector(writeLimitDate), for: .valueChanged)
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        dateFormatter.dateFormat = "yyyy年M月d日"
+        dateLabel.text = dateFormatter.string(from: datePicker.date)
     }
     
 
@@ -27,6 +32,7 @@ class BuffModalViewController: UIViewController {
     }
     */
 
+    @IBOutlet weak var dateLabel: CustomUILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
     
     @IBAction func doneButtonTapped(_ sender: Any) {
@@ -35,9 +41,12 @@ class BuffModalViewController: UIViewController {
     @IBAction func cancelButtonTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    
-    @objc func writeLimitDate(){
-        print()
+    @IBAction func dateChanged(_ sender: Any) {
+        dateLabel.text = dateFormatter.string(from: datePicker.date)
     }
+    
+    
+//    @objc func writeLimitDate(){
+//        print()
+//    }
 }
